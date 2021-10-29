229dcc1278c979cf955f3114df0b10079522c631
Tyagi05 <93199722+Tyagi05@users.noreply.github.com>
2021-10-29 16:23:40 +0530

    second commit

---
+++ src/com/headfirst/one.java
@@ -0,0 +1,14 @@
+package com.headfirst;
+
+public class one {
+    public static void main(String[] args){
+        int x= 10;
+        while(x<11){
+            x=x-1;
+            if(x==0){
+                break;
+            }
+            System.out.println(x);
+        }
+    }
+}
