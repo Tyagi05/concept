fb24f4a4e84b0f6c9d323496ecf0587324a0b12d
Tyagi05 <93199722+Tyagi05@users.noreply.github.com>
2021-10-29 18:19:02 +0530

    Oct 29

---
+++ src/com/headfirst/Movie.java
@@ -0,0 +1,12 @@
+package com.headfirst;
+
+public class Movie {
+    int size;
+    String color;
+    public static void main(String[] args){
+        Movie m= new Movie();
+        m.color = "black";
+        m.size=40;
+      
+    }
+}
