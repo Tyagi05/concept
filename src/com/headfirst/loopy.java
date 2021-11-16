ff7db0c5b60d9f2cc2f28b16b6b7f2da27ea4c7d
Tyagi05 <93199722+Tyagi05@users.noreply.github.com>
2021-11-16 17:07:32 +0530

    third commit

---
+++ src/com/headfirst/loopy.java
@@ -0,0 +1,18 @@
+package com.headfirst;
+
+public class loopy {
+
+    public static void main(String[] args) {
+        int x=1;
+       System.out.println("before the loop");
+       while(x<4){
+           System.out.println("in the loop");
+           x+=1;
+
+       }
+       System.out.println("after the loop");
+    }
+    }
+
+
+
