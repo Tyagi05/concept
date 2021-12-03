e8f08800b69032ec93fb75c41d1d44ec12d132e0
Tyagi05 <93199722+Tyagi05@users.noreply.github.com>
2021-12-03 15:24:14 +0530

    ninth commit

--- src/com/headfirst/chapter16/Desc.java
+++ src/com/headfirst/chapter16/Desc.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Desc {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        ArrayList<String> as= new ArrayList<>();
-        as.add("Orange");
-        as.add("Apple");
-        as.add("Mango");
+        ArrayList<String> as = new ArrayList<String>();
+        as.add("APPLE");
+        as.add("CHEEKU");
+        as.add("BANANA");
+
 
         Collections.sort(as, Collections.reverseOrder());
-        for(String a:as){
+        for (String a : as) {
             System.out.println(a);
         }
     }
